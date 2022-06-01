3f7d8a183e1ced34359f6b619e22f9bd85453af0
User <User@ADMIN>
2022-06-01 13:21:37 +0300

    Добавьте файлы проекта.

---
+++ Homework 31.05.22.cpp
@@ -0,0 +1,137 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "UKR");
+
+	cout << "Укажите количество очков здоровья дракона: ";
+	int dragonhealth;
+	cin >> dragonhealth;
+	
+	cout << "Укажите количество очков атаки дракона: ";
+	int dragonattack;
+	cin >> dragonattack;
+
+	cout << "Укажите количество очков здоровья копейщика: ";
+	int spearhealth;
+	cin >> spearhealth;
+
+	cout << "Укажите количество очков атаки копейщика: ";
+	int spearattack;
+	cin >> spearattack;
+
+	int battledragonhealth = dragonhealth;
+	int battlespearhealth = spearhealth;
+
+	int battlespearattack = spearattack;
+
+	int resultbattlespearhealth = 0;
+
+	int temp = 0;
+
+	int numspear = 1;
+
+	int modifier = dragonhealth / 34;
+
+	int spear = spearhealth;
+
+	while (true)
+	{
+		battledragonhealth -= battlespearattack;
+		battlespearhealth -= dragonattack;
+		temp += dragonattack;
+		spearhealth = spear;
+
+		if (battlespearhealth <= 0)
+		{
+			battlespearhealth = spearhealth * numspear;
+			battlespearhealth += spearhealth;
+			numspear += 1 * modifier;
+			battlespearattack = spearattack * numspear;
+			battledragonhealth = dragonhealth;
+		}
+		if (temp > spearhealth)
+		{
+			numspear -= (temp / spearhealth);
+			battlespearhealth -= spearhealth * (temp / spearhealth);
+			battlespearattack -= spearattack * (temp / spearhealth);
+			temp -= spearhealth * (temp / spearhealth);
+		}
+		if (temp < spearhealth && temp > 0)
+		{
+			spearhealth -= temp;
+		}
+		if (battledragonhealth <= 0)
+		{
+			break;
+		}
+		temp = 0;
+	}
+
+	double num = dragonattack / spear;
+
+	if (battledragonhealth <= 0 && numspear > 0)
+	{
+		battledragonhealth = dragonhealth;
+		battlespearhealth = spearhealth * numspear;
+		battlespearattack = spearattack * numspear;
+
+		while (true)
+		{
+			battledragonhealth -= battlespearattack;
+			cout << "\nКопейщики атакуют (урон " << battlespearattack << ") - у дракона осталось " << battledragonhealth << " очков здоровья.\n";
+			if (battledragonhealth > 0)
+			{
+				battlespearhealth -= dragonattack;
+				temp += dragonattack;
+				cout << "Дракон атакует (урон " << dragonattack << ") - осталось копейщиков: " << (battlespearattack / 10) - num << "\n\n";
+				if (spearhealth < spear)
+				{
+					cout << "Один из копейщиков ранен (осталось очков здоровья: " << spearhealth << ").\n\n";
+				}
+			}
+
+			spearhealth = spear;
+
+			if (temp > spearhealth)
+			{
+				numspear -= (temp / spearhealth);
+				battlespearhealth -= spearhealth * (temp / spearhealth);
+				battlespearattack -= spearattack * (temp / spearhealth);
+				temp -= spearhealth * (temp / spearhealth);
+			}
+			if (temp < spearhealth && temp > 0)
+			{
+				spearhealth -= temp;
+			}
+			if (dragonattack < spear && spearhealth == temp)
+			{
+				numspear -= 1;
+				battlespearhealth -= spearhealth;
+				battlespearattack -= spearattack;
+			}
+			if (battledragonhealth <= 0)
+			{
+				cout << "\nПобеда\n";
+				int result = (battlespearattack / 10) - num;
+				if (result == 0)
+				{
+					result = 1;
+				}
+				else if (result > 0)
+				{
+					result += num;
+				}
+				if (spear > dragonattack)
+				{
+					result++;
+				}
+				cout << "Осталось копейщиков в живых: " << result<< "\n";
+				break;
+			}
+			temp = 0;
+		}
+	}
+}
+
